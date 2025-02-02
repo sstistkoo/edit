c3f55f42ecc7bd4ddf54c4d68a3540c6aa1b4e95
stistko <65913642+sstistkoo@users.noreply.github.com>
2025-02-02 10:35:14 +0100

    Update vite.config.js

--- cnc_editor/vite.config.js
+++ cnc_editor/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/edit/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000
