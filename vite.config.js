02cd461e1f2ce1ba168791a72eeef4c813b08c9d
sstistkoo <Stistko.petr@seznam.cz>
2025-02-02 10:54:00 +0100

    Aktualizace pro mobilní zobrazení

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/edit/',
+  base: '/edit/cnc_editor/',  // upravená cesta
   plugins: [react()],
   server: {
     port: 3000
